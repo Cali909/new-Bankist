74474e1456d6f3afee11653e3c9119e63d01adbf
Alireza Garshasbi <alirezagarshasbi021@gmail.com>
2022-08-20 23:52:24 +0430

    define general button styles. style the 'open account' button in the navBar.

--- index.js
+++ index.js
@@ -2,10 +2,20 @@
 
 const navTogglerButton = document.getElementById('Capa_1');
 const navLinks = document.querySelector('.nav__links');
+// const navBarContainer = document.querySelector('.nav');
 
 const handleNavToggle = () => {
   navLinks.classList.toggle('expanded');
   navTogglerButton.classList.toggle('expanded');
 };
 
+// const handleHover = e => {
+//   if (e.target.classList.contains('nav__link')) {
+//     navBarContainer.style.opacity = 0.8;
+//     e.target.style.opacity = 1;
+//   }
+// };
+
 navTogglerButton.addEventListener('click', handleNavToggle);
+
+// navBarContainer.addEventListener('mouseover', handleHover);
