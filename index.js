93bdb7b4fad2a90950181c128adecc02bd8eed4c
Alireza Garshasbi <alirezagarshasbi021@gmail.com>
2022-08-22 02:03:48 +0430

    build the modal and overlay. make it responsive. work upon clicks.

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 'use strict';
 
+//DEFINITIONS
+const modalEl = document.querySelector('.modal');
+const overlayEl = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.btn--close-modal');
 const navTogglerButton = document.getElementById('Capa_1');
 const navLinks = document.querySelector('.nav__links');
+const ctaBtns = document.querySelectorAll('#btn--cta');
 // const navBarContainer = document.querySelector('.nav');
+
+//FUNCTIONS
+const handleShowModal = () => {
+  overlayEl.classList.remove('hidden');
+  modalEl.classList.remove('hidden');
+};
+
+const handleCloseModal = () => {
+  overlayEl.classList.add('hidden');
+  modalEl.classList.add('hidden');
+};
 
 const handleNavToggle = () => {
   navLinks.classList.toggle('expanded');
@@ -15,6 +31,16 @@ const handleNavToggle = () => {
 //     e.target.style.opacity = 1;
 //   }
 // };
+
+//EVENTLISTENERS
+ctaBtns.forEach(btn => btn.addEventListener('click', handleShowModal));
+overlayEl.addEventListener('click', handleCloseModal);
+closeModalBtn.addEventListener('click', handleCloseModal);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modalEl.classList.contains('hidden')) {
+    handleCloseModal();
+  }
+});
 
 navTogglerButton.addEventListener('click', handleNavToggle);
 
